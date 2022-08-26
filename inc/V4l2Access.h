a3b55292312314474ccc433e6a8e3caf61e82155
csenler <csenler@hotmail.com>
2022-08-26 12:26:33 +0000

    format

--- inc/V4l2Access.h
+++ inc/V4l2Access.h
@@ -4,11 +4,10 @@
 ** any purpose.
 **
 ** V4l2Access.h
-** 
-** V4L2 wrapper 
+**
+** V4L2 wrapper
 **
 ** -------------------------------------------------------------------------*/
-
 
 #ifndef V4L2_ACCESS
 #define V4L2_ACCESS
@@ -17,35 +16,36 @@
 
 class V4l2Access
 {
-	public:		
-		V4l2Access(V4l2Device* device);
-		virtual ~V4l2Access();
-		
-		int getFd()                  { return m_device->getFd();         }
-		unsigned int getBufferSize() { return m_device->getBufferSize(); }
-		unsigned int getFormat()     { return m_device->getFormat();     }
-		unsigned int getWidth()      { return m_device->getWidth();      }
-		unsigned int getHeight()     { return m_device->getHeight();     }
-		
-		void queryFormat()  { m_device->queryFormat();          }
-		int setFormat(unsigned int format, unsigned int width, unsigned int height)  { 
-			return m_device->setFormat(format, width, height); 
-		}
-		int setFps(int fps) {
-			return m_device->setFps(fps);
-		}
+public:
+	V4l2Access(V4l2Device *device);
+	virtual ~V4l2Access();
 
-		int isReady()       { return m_device->isReady();       }
-		int start()         { return m_device->start();         }
-		int stop()          { return m_device->stop();          }
+	int getFd() { return m_device->getFd(); }
+	unsigned int getBufferSize() { return m_device->getBufferSize(); }
+	unsigned int getFormat() { return m_device->getFormat(); }
+	unsigned int getWidth() { return m_device->getWidth(); }
+	unsigned int getHeight() { return m_device->getHeight(); }
 
-	private:
-		V4l2Access(const V4l2Access&);
-		V4l2Access & operator=(const V4l2Access&);
-	
-	protected:
-		V4l2Device* m_device;		
+	void queryFormat() { m_device->queryFormat(); }
+	int setFormat(unsigned int format, unsigned int width, unsigned int height)
+	{
+		return m_device->setFormat(format, width, height);
+	}
+	int setFps(int fps)
+	{
+		return m_device->setFps(fps);
+	}
+
+	int isReady() { return m_device->isReady(); }
+	int start() { return m_device->start(); }
+	int stop() { return m_device->stop(); }
+
+private:
+	V4l2Access(const V4l2Access &);
+	V4l2Access &operator=(const V4l2Access &);
+
+protected:
+	V4l2Device *m_device;
 };
-
 
 #endif
